43302d0fa58e130606c5a6343fc99a444a76ac9c
tzur galant <tzurgalant@gmail.com>
2026-04-30 22:05:47 +0300

    add JsonResponsePacketSerializer login func

--- JsonResponsePacketSerializer.cpp
+++ JsonResponsePacketSerializer.cpp
@@ -1,17 +1,34 @@
 #include "JsonResponsePacketSerializer.h"
-#include "json.hpp"         
-
-
+#include "json.hpp" // ספריית nlohmann/json
+#include <vector>
+#include <string>
 
 using json = nlohmann::json;
 
-Buffer JsonResponsePacketSerializer::serializeResponse(LoginResponse req)
+std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(LoginResponse res)
 {
-    Buffer b;
-    
-}
+    json j;
+    j["status"] = res.status;
 
-Buffer JsonResponsePacketSerializer::serializeResponse(SignupResponse req)
-{
+    std::string jsonStr = j.dump();
 
+    Buffer buffer;
+
+    //msg code
+    buffer.push_back(101);
+
+    unsigned int length = jsonStr.length();
+
+    //פירוק ל4 בייטים
+    buffer.push_back((length >> 24) & 0xFF);
+    buffer.push_back((length >> 16) & 0xFF);
+    buffer.push_back((length >> 8) & 0xFF);
+    buffer.push_back(length & 0xFF);
+
+    for (char c : jsonStr)
+    {
+        buffer.push_back((unsigned char)c);
+    }
+
+    return buffer;
 }
