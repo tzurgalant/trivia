a12a9b0e70909d89b54572e90a73eb4f8f485997
drorMagshimimPc <dror3646@gmail.com>
2026-05-13 20:48:09 +0300

    start my misshan:'do the serialize of the out reponse

--- JsonResponsePacketSerializer.h
+++ JsonResponsePacketSerializer.h
@@ -10,5 +10,13 @@ class JsonResponsePacketSerializer
 public:
 	static Buffer serializeResponse(LoginResponse req);
 	static Buffer serializeResponse(SignupResponse req);
+	static Buffer serializerReqponse(ErrorResponse req);
+	static Buffer serializerReqponse(LogoutResponse req);
+	static Buffer serializerReqponse(GetRoomsResponse req);
+	static Buffer serializerReqponse(GetPlayersInRoomResponse req);
+	static Buffer serializerReqponse(JoinRoomResponse req);
+	static Buffer serializerReqponse(CreateRoomResponse req);
+	static Buffer serializerReqponse(GetHighScoreResponse req);
+	static Buffer serializerReqponse(GetPersonalStatsReponse req);
 private:
 };
